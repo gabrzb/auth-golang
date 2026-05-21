596b47227c3d6b5efd593e7aab8e88498720a7f7
gabrzb <gabrielzbz.gz@gmail.com>
2026-05-21 19:51:59 -0300

    feat: connect to postgres using gorm

---
+++ backend/internal/database/postgres.go
@@ -0,0 +1,25 @@
+package database
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/gabrzb/auth-go-gin/internal/config"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+func Connect(cfg *config.Config) *gorm.DB {
+	dsn := fmt.Sprintf(
+		"host=%s port=%s user=%s password=%s dbname=%s sslmode=disable",
+		cfg.DBHost, cfg.DBPort, cfg.DBUser, cfg.DBPassword, cfg.DBName,
+	)
+
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		log.Fatalf("Failed to connect to database: %v", err)
+	}
+
+	log.Println("Database connected")
+	return db
+}
